298993d9e6f5a54ed8bddee6eae4ba75c11f1fb1
NetStriKerX <lamp.f@hotmail.com>
2023-07-21 17:00:44 +0700

    feat(review): create review model belongs to book and migrate db

--- db/migrate/20230720080631_create_books.rb
+++ db/migrate/20230720080631_create_books.rb
@@ -1,9 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
-      t.string :name
-      t.text :description
-      t.datetime :release
+      t.string :name, null: false
+      t.text :description, null: false
+      t.datetime :release, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_20_080631) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_21_041559) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "release"
+    t.string "name", null: false
+    t.text "description", null: false
+    t.datetime "release", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment", null: false
+    t.float "star", null: false
+    t.bigint "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
+  end
+
+  add_foreign_key "reviews", "books"
 end
