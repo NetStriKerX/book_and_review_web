f63f9ebc3df34c69fe7f0c5c42077922ef312369
NetStriKerX <lamp.f@hotmail.com>
2023-07-21 17:00:44 +0700

    feat(create-book): create controller and form for create new book + validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,23 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book
+    else
+      redirect_to new_book_path(error: @book.errors.full_messages)
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :description, :release)
+  end
 end
