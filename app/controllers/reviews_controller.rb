61c85ad390c9c70fd652def64a4e475ef065f0d5
NetStriKerX <lamp.f@hotmail.com>
2023-07-21 17:36:05 +0700

    feat: add function to delete reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: %i[edit update destroy]
   def create
     @book = Book.find(params[:book_id])
     @review = @book.reviews.create(review_params)
@@ -6,19 +7,26 @@ class ReviewsController < ApplicationController
   end
 
   def edit
-    @review = Review.find(params[:id])
     @book = @review.book
   end
 
   def update
-    @review = Review.find(params[:id])
     @review.update(review_params)
     redirect_to book_path(@review.book)
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to @review.book
   end
 
   private
 
   def review_params
     params.require(:review).permit(:comment, :star)
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
   end
 end
